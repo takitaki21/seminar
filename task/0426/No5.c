a4ceb838daa39613125b246e109488e8d62470eb
takitaki21 <hrxdr280@yahoo.co.jp>
2016-05-09 18:14:24 +0900

    課題５　作成
    ｎCrを求める

---
+++ task/0426/No5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+ {
+  int n ,r  ;
+  
+  printf("n=");
+scanf("%d",&n);
+  printf("r=");
+scanf("%d",&r);
+  
+  int n1 ,r1,n2,r2, k1, k2;
+n2=1;
+r2=1;
+k2=1;
+   for (n1 = n ; n1 >= 1  ; n1--){
+
+n2=n2*n1;
+   }
+   for (r1 = r; r1 >= 1 ; r1--){
+r2=r2*r1 ;
+  }
+   for (k1 = n-r; k1>=1; k1-- ){
+    k2=k2*k1 ;
+   }
+   printf("nCr = %d\n",n2 /r2/k2);
+}
+
