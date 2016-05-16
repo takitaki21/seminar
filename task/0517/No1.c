cb3ea4badf633fa2c17e86668ce2969ffcf1d111
takitaki21 <hrxdr280@yahoo.co.jp>
2016-05-16 20:01:54 +0900

    0517 課題１　作成
    配列を用いらずミルクショップ

---
+++ task/0517/No1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main (void){
+		srand((unsigned)time(NULL)); 
+			int a0=rand()%60+1,a1=rand()%60+1,a2=rand()%60+1,a3=rand()%60+1,a4=rand()%60+1,a5=rand()%60+1,a6=rand()%60+1,a7=rand()%60+1,a8=rand()%60+1,a9=rand()%60+1;
+			printf("%d\n",a0);
+			printf("%d\n",a1);
+			printf("%d\n",a2);
+			printf("%d\n",a3);
+			printf("%d\n",a4);
+			printf("%d\n",a5);
+			printf("%d\n",a6);
+			printf("%d\n",a7);
+	        printf("%d\n",a8);
+			printf("%d\n",a9 );  
+ int t;
+    
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+   	if(a4 < a5){t = a4;a4=a5; a5=t;}
+    if(a5 < a6){t = a5;a5=a6; a6=t;}
+    if(a6 < a7){t = a6;a6=a7; a7=t;}
+    if(a7 < a8){t = a7;a7=a8; a8=t;}
+    if(a8 < a9){t = a8;a8=a9; a9=t;}
+
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+   	if(a4 < a5){t = a4;a4=a5; a5=t;}
+    if(a5 < a6){t = a5;a5=a6; a6=t;}
+    if(a6 < a7){t = a6;a6=a7; a7=t;}
+    if(a7 < a8){t = a7;a7=a8; a8=t;}
+ 
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+   	if(a4 < a5){t = a4;a4=a5; a5=t;}
+    if(a5 < a6){t = a5;a5=a6; a6=t;}
+    if(a6 < a7){t = a6;a6=a7; a7=t;}
+
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+   	if(a4 < a5){t = a4;a4=a5; a5=t;}
+    if(a5 < a6){t = a5;a5=a6; a6=t;}
+
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+   	if(a4 < a5){t = a4;a4=a5; a5=t;}
+
+   	if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+    if(a3 < a4){t = a3;a3=a4; a4=t;}
+
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+    if(a2 < a3){t = a2;a2=a3; a3=t;}
+
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+    if(a1 < a2){t = a1;a1=a2; a2=t;}
+   
+    if(a0 < a1){t = a0;a0=a1; a1=t;}
+  printf("%d %d %d %d %d %d %d %d %d %d\n",a9,a8,a7,a6,a5,a4,a3,a2,a1,a0);
+
+   printf("\n") ;
+            printf("%d\n",a9*0);
+			printf("%d\n",a9+a8);
+			printf("%d\n",a8+a7);
+			printf("%d\n",a7+a6);
+			printf("%d\n",a6+a5);
+			printf("%d\n",a5+a4);
+			printf("%d\n",a4+a3);
+			printf("%d\n",a3+a2);
+	        printf("%d\n",a2+a1);
+			printf("%d\n",a1+a0 );  
+  printf("custum wait time =%d\n",a1+a2+a3+a4+a5+a6+a7+a8+a9) ;
+}
