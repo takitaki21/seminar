ffa57b07c5951f03bc360e9d9c0e55d5ed19214b
takitaki21 <hrxdr280@yahoo.co.jp>
2016-05-16 18:39:01 +0900

    課題６
    武田君に言われて１０→９

--- task/0426/No6.c
+++ task/0426/No6.c
@@ -5,7 +5,7 @@ int main(void){
   int i,j,x,min;
   printf("please input 3 numbers\n");
   for(i=0 ;i<10; i++) scanf("%d",&a[i]);
-         for(i=0; i<10; i++){
+         for(i=0; i<9; i++){
            for(j=i+1; j<10; j++){
 
                 if(a[i]>a[j]){
